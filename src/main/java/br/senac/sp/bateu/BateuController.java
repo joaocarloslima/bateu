add7448930ccf89a0e15d95a557c5be85f14082c
joaocarloslima <joaocarloslima@me.com>
2025-02-20 20:41:35 -0300

    refactor BateuController: update comments and uncomment method stubs

--- src/main/java/br/senac/sp/bateu/BateuController.java
+++ src/main/java/br/senac/sp/bateu/BateuController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BateuController {
 
-    //declare e instancie um objeto do tipo baralho
+    // declare e instancie um atribulo do tipo baralho
     private int cont = 0;
     
     @GetMapping
@@ -16,25 +16,25 @@ public class BateuController {
         return "index"; 
     }
 
-    // @GetMapping("virar")
-    // public String virar(Model model) {
-    //     if (baralho.getCartas().isEmpty()) {
-    //         model.addAttribute("msg", "Acabaram as cartas!");
-    //         return "index";
-    //     }
+    @GetMapping("virar")
+    public String virar(Model model) {
+        // if (baralho.getCartas().isEmpty()) {
+        //     model.addAttribute("msg", "Acabaram as cartas!");
+        //     return "index";
+        // }
 
-    //     cont++;
-    //     if (cont > 13) cont = 1;
-    //     model.addAttribute("cont", cont);
-    //     model.addAttribute("carta", baralho.comprar());
-    //     return "index";
-    // }
+        // cont++;
+        // if (cont > 13) cont = 1;
+        // model.addAttribute("cont", cont);
+        // model.addAttribute("carta", baralho.comprar());
+        return "index";
+    }
 
-    // @GetMapping("novo")
-    // public String novo(Model model) {
-    //     cont = 0;
-    //     baralho = new Baralho();
-    //     return "index";
-    // }
+    @GetMapping("novo")
+    public String novo(Model model) {
+        // cont = 0;
+        // baralho = new Baralho();
+        return "index";
+    }
 
 }
